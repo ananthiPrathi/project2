c8892b228c5d2d73b5b7f0ed659f622d2f5f31e6
Ananthi <118195006+ananthiPrathi@users.noreply.github.com>
2025-01-28 22:59:50 -0800

    Create Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,19 @@
+puvlic class datatype{
+  public static void main(String[] args)
+  {
+    byte b =20;
+    short s  = 40;
+    int i = 100;
+    long l = 9000;
+    double d = 34.78;
+    float f = 3.5649f;
+    char c = 'c';
+    
+    System.out.println(b);
+     System.out.println(s);
+     System.out.println(i);
+     System.out.println(l);
+     System.out.println(d);
+     System.out.println(f);
+     System.out.println(c);
+  }
